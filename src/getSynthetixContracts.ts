aba03aaabd09c965dd6a748ada2f54ffec72648c
Evgeny Boxer <evgeny.boxer@gmail.com>
2020-08-06 02:26:25 +1000

    fix: various fixes (types/eslint rules/tsconfig/etc) (#1)
    * fix: various fixes (types/eslint rules/tsconfig/etc)
    
    * fix: test

--- src/getSynthetixContracts.ts
+++ src/getSynthetixContracts.ts
@@ -4,24 +4,34 @@ import { ethers } from 'ethers';
 
 import { Networks } from './types';
 
-export default function (
-	network: Networks,
-): Array<{
+type Target = {
+	name: string;
+	source: string;
+	address: string;
+};
+
+type ContractDefinition = {
 	name: string;
 	abi: ethers.ContractInterface;
 	address: string;
-}> {
+};
+
+type ContractDefinitions = ContractDefinition[];
+
+const getSynthetixContracts = (network: Networks): ContractDefinitions => {
 	if (!Object.values(Networks).includes(network)) {
 		throw new Error('unsupported network passed');
 	}
 	const sources = snx.getSource({ network });
 	const targets = snx.getTarget({ network });
 
-	return Object.values(targets).map(
-		({ name, source, address }: { name: string; source: string; address: string }) => ({
-			name,
-			abi: sources[source].abi,
-			address,
-		}),
-	);
-}
+	// TODO: figure out what's going on here
+	// @ts-ignore
+	return Object.values(targets).map(({ name, source, address }: Target) => ({
+		name,
+		abi: sources[source].abi,
+		address,
+	}));
+};
+
+export default getSynthetixContracts;
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,30 @@
 import { ethers } from 'ethers';
-import getSynthetixContracts from './getSynthetixContracts';
-import { Config } from './types';
-import { createContractInstance } from './createContractInstance';
 
-export default function ({ network, signer, provider }: Config) {
+import getSynthetixContracts from './getSynthetixContracts';
+import createContractInstance from './createContractInstance';
+
+import { Config } from './types';
+
+// TODO: consider using a class?
+
+const init = ({
+	network,
+	signer,
+	provider,
+}: Config): {
+	// TODO: type this
+	synthetixContracts: Record<string, ethers.Contract>[];
+	utils: typeof ethers.utils;
+} => {
 	const contractData = getSynthetixContracts(network);
 	const synthetixContracts = contractData.map(({ name, abi, address }) => ({
 		[name]: createContractInstance(address, abi, provider || signer),
 	}));
+
 	return {
 		synthetixContracts,
 		utils: ethers.utils,
 	};
-}
+};
+
+export default init;

f007008cf903def907e5ab57ad8a80e7ae416d95
Evgeny Boxer <evgeny.boxer@gmail.com>
2020-08-05 22:11:40 +1000

    fix: test

--- test/createContractInstance.test.js
+++ test/createContractInstance.test.js
@@ -1,17 +1,17 @@
-import { createContractInstance } from '../src/createContractInstance';
+import createContractInstance from '../src/createContractInstance';
 import { randomContract } from './mockData';
 
 describe('Checks creating new contract instances using ethers', function () {
-  let noProviderContract;
-  beforeAll(() => {
-    noProviderContract = createContractInstance(randomContract.address, randomContract.abi);
-  });
+	let noProviderContract;
+	beforeAll(() => {
+		noProviderContract = createContractInstance(randomContract.address, randomContract.abi);
+	});
 
-  test('works properly on the random test contract', function () {
-    expect(typeof noProviderContract.get_dy_underlying).toBe('function');
-  });
+	test('works properly on the random test contract', function () {
+		expect(typeof noProviderContract.get_dy_underlying).toBe('function');
+	});
 
-  test('does not return methods that are undefined', function () {
-    expect(typeof noProviderContract.method_not_included).toBe('undefined');
-  });
+	test('does not return methods that are undefined', function () {
+		expect(typeof noProviderContract.method_not_included).toBe('undefined');
+	});
 });

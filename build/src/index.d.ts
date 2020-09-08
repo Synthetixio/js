1f70cb472aed7bf078e84625c8edf43edddf059a
dvd-schwrtz <dbschwartz17@gmail.com>
2020-09-08 14:17:32 -0700

    include build files

---
+++ build/src/index.d.ts
@@ -0,0 +1,5 @@
+import { Config, Network, NetworkId, Target, TargetsRecord, ContractsMap, SynthetixJS, Synth, Token } from './types';
+declare const synthetix: ({ networkId, network, signer, provider }: Config) => SynthetixJS;
+export { synthetix, Network, NetworkId };
+export type { Config, Target, TargetsRecord, ContractsMap, SynthetixJS, Synth, Token };
+export default synthetix;
